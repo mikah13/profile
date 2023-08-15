b82f32696f20b4556c3a47b8aab21a141796077e
opacteam <mike@minisisinc.com>
2023-08-15 06:19:21 -0700

    added appbar

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Test = (props: Props) => {
+  return <div>Hello World</div>;
+};
+
+export default Test;
---
+++ src/components/appbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+type Props = {};
+
+const Appbar = (props: Props) => {
+  return (
+    <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
+      <div className="max-w-8xl mx-auto">
+        <div className="py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
+          <div className="relative flex items-center">
+            <Link href="/">Homepage</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Appbar;
