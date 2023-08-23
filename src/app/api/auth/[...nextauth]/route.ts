f5c6265fb34407819ee78af1aac910da455b3068
mikah13 <anhminhhoang13@gmail.com>
2023-08-23 08:05:11 -0700

    push master

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -25,22 +25,20 @@ export const nextAuthOptions = {
     }),
     // ...add more providers here
   ],
-  useCallbacks: {
+  callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
       if (account) {
-        token.accessToken = account.access_token
+        token.accessToken = account.access_token;
       }
-      return token
+      return token;
     },
     async session({ session, token, user }) {
       // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken
-      return session
-    }
-  }
-
-  
+      // session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 } satisfies AuthOptions;
 const handler = NextAuth(nextAuthOptions);
 export { handler as GET, handler as POST };
