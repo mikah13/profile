7c46327ec8fc053806aba709d11c2718e8310ed4
mikah13 <anhminhhoang13@gmail.com>
2023-08-30 06:25:45 -0700

    update prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 export type * from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
